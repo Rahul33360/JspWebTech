91b2d3175e84d50ca2f11d443411ef0e29f2c430
Rahul33360 <109918304+Rahul33360@users.noreply.github.com>
2025-10-18 12:26:04 +0530

    modules (import and export)

---
+++ JS/Modules/Example1/three.js
@@ -0,0 +1,14 @@
+function add(a,b) {
+    return a+b;
+}
+
+let newVar = 'Anything3223'
+
+
+function sub(a,b) {
+    return a-b;
+}
+
+export default sub;     // default type export
+
+export {add,newVar};      // this onen is name export
---
+++ JS/Modules/Example1/two.js
@@ -0,0 +1,12 @@
+function multiply(a,b) {
+    return a*b;
+}
+
+export let msg = 'Anything3223' // named export if we export like this 
+
+
+export function divide(a,b) {       // name exprot 
+    return a/b;
+}
+
+export default multiply;     // default type export. in a file(module) only one default module can live 
